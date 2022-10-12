4ae4bc9cd70c62c29a9ad1c9396eae5e2cba098d
abdulatif-fs <fasijardiq@gmail.com>
2022-10-12 22:06:16 +0700

    tampilan selain index

--- server.py
+++ server.py
@@ -1,19 +1,40 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
+        print('path = ', self.path)
         if self.path == '/':
             self.path = '/index.html'
-        
-        try:
+
             file_to_open = open(self.path[1:]).read()
             self.send_response(200)
-        except:
-            file_to_open = 'File Not Found'
-            self.send_response(404)
+
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open, 'utf-8'))
+
+        elif os.path.isdir(self.path):
         
-        self.end_headers()
-        self.wfile.write(bytes(file_to_open, 'utf-8'))
+            try:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(str(os.listdir(self.path)).encode())
+            except:
+                file_to_open = 'File Not Found'
+                self.send_response(404)
+        
+        else:
+            try:
+                with open(self.path, 'rb') as f:
+                    data = f.read()
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(data)
+            # error handling skipped
+            except Exception:
+                self.send_response(404)
+                self.end_headers()
+                self.wfile.write(b'File Not Found')
 
 httpd = HTTPServer(('localhost', 8080), Server)
 httpd.serve_forever()
