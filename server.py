ab38673cf33158aeff4975067c3313b3319d916f
abdulatif-fs <fasijardiq@gmail.com>
2022-10-19 14:46:32 +0700

    berhasil download pdf

--- server.py
+++ server.py
@@ -4,46 +4,61 @@ import http.server
 import socketserver
 
 
-Path = 'C:/Users/ASUS/OneDrive/Dokumen/Abdulatif/kuliah/sem9/progjar/WebServer'
+Path = 'C:/Users/ASUS/OneDrive/Dokumen/Abdulatif/kuliah/sem9/progjar/WebServer/utama/'
+newdir = os.path.dirname(Path)
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
+        filename= self.path.replace("/", '')
          
         print('path = ', self.path)
-        print(os.path.join(Path, self.path))
-        dir_list = os.listdir(self.path)
-        print('dirlist = ', dir_list)
-        print()
+        # print(os.path.join(Path, self.path))
+        # dir_list = os.listdir(self.path)
+        # print('dirlist = ', dir_list)
+        # print()
         if self.path == '/':
             self.path = '/index.html'
 
-            file_to_open = open(self.path[1:]).read()
+            file_to_open = open(newdir+'/'+self.path[1:]).read()
             self.send_response(200)
 
             self.end_headers()
             self.wfile.write(bytes(file_to_open, 'utf-8'))
 
-        elif os.path.isdir(self.path):
+        elif filename not in os.listdir(newdir):
         
-            try:
+            self.path = '/404.html'
+
+            file_to_open = open(newdir+self.path).read()
+            self.send_response(404)
+
+            self.end_headers()
+            self.wfile.write(bytes(file_to_open, 'utf-8'))
+        
+        elif '.' in self.path:
+            if "html" in self.path:
+                file_to_open = open(newdir+self.path).read()
                 self.send_response(200)
+
                 self.end_headers()
-                self.wfile.write(str(os.listdir(self.path)).encode())
-            except:
-                file_to_open = 'File Not Found'
-                self.send_response(404)
-        
-        else:
-            try:
-                with open(self.path, 'rb') as f:
+                self.wfile.write(bytes(file_to_open, 'utf-8'))
+            else:
+                with open(newdir+self.path, 'rb') as f:
                     data = f.read()
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(data)
-            # error handling skipped
-            except Exception:
-                self.send_response(404)
-                self.end_headers()
-                self.wfile.write(b'File Not Found')
+                    self.send_response(200)
+                    self.end_headers()
+                    self.wfile.write(data)
+        else:
+        # try:
+            # with open(newdir+self.path, 'rb') as f:
+            #     data = f.read()
+            #     self.send_response(200)
+            #     self.end_headers()
+            #     self.wfile.write(data)
+        # error handling skipped
+        # except Exception:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b'Folder menyusul')
 
 httpd = HTTPServer(('localhost', 8080), Server)
 httpd.serve_forever()
